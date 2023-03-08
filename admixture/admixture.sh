ac3b88d0d8af1807254dfe7320d9e33497f8d571
KongyangZhu <cewinhot@hotmail.com>
2023-03-08 21:18:21 +0800

    version v2.7

--- admixture/admixture.sh
+++ admixture/admixture.sh
@@ -1,29 +1,44 @@
 #!/bin/sh
-
-
 # coding:utf-8
-# @Time : 2022/9/28 16:30
-# @Version: 2.6
-# @File : admixture.2.6.sh
-# @Author : zky
+# @Time : 2023/03/08 16:30
+# @Version: 2.7
+# @File : admixture.sh
+# @Author : KyanZhu
 
 
 # SETTINGS
 geno_dir=/home/KongyangZhu/beizhou/5.popgen/5.merged_dataset/HO
+work_dir=/home/KongyangZhu/beizhou/5.popgen/7.admixture
 geno_file=Wudi_Xianbei_HO  # prefix
-workdir=/home/KongyangZhu/beizhou/5.popgen/7.admixture
-extract_poplist=poplist.txt
+poplist=poplist.txt  # extract poplist
 fancy=/home/KongyangZhu/sh/fancy_admixture
-remove_excess=/home/KongyangZhu/sh/admixture/remove_excess.py
-thread=5
-K_st=1
-K_en=10
+thread=10
+K_st=2
+K_en=14
 bootstrap="-B100"  # 不需要bootstrap放空""
 
-cd ${workdir}
+mkdir -p ${work_dir} ; cd ${work_dir}
+
+# 0. check files
+missing=0
+if [ ! -f ${geno_dir}/${geno_file}.geno ];then echo "!!! Missing geno file !!! " ; missing=1 ; fi
+if [ ! -f ${poplist} ];then echo "!!! Missing poplist !!! " ; missing=1 ; fi    
+if [ ! -f remove_excess.py ];then echo "!!! remove_excess.py !!! " ; missing=1 ; fi
+if [ ${missing} -eq 1 ];then exit; fi
+
+# 0. check extract.poplist
+echo -e "=== checking popluations ! ==="
+lack_pops=""
+cat ${poplist} | egrep -v "#|=" > extract.poplist
+cat ${geno_dir}/${geno_file}.ind | awk '{print $3}' | sort -u > ind.tmp
+pops=$(cat extract.poplist)
+for pop in ${pops};do
+    cat ind.tmp | grep -x ${pop} >/dev/null 2>&1
+    if [ ! $? -eq 0 ];then lack_pops="${lack_pops} [${pop}]" ; flag="FALSE"; fi
+done && echo -e "=== check poplist done ! ===\n\n" ; rm ind.tmp
+if [[ ${flag} == "FALSE" ]];then echo ${lack_pops} not in dataset; exit; fi
 
 # 1. extract poplist
-cat ${extract_poplist} | grep -v "=" > extract.poplist
 for i in "extract.par";do
     echo "genotypename: ${geno_dir}/${geno_file}.geno"
     echo "snpname: ${geno_dir}/${geno_file}.snp"
@@ -33,11 +48,11 @@ for i in "extract.par";do
     echo "snpoutname: tmp.snp"
     echo "indivoutname: tmp.ind"
     echo "poplistname: extract.poplist"
+    echo "hashcheck: NO"
 done > extract.par ; convertf -p extract.par ; echo ""
 
 # 2. remove individual larger than MAX
 MAX=15
-cp ${remove_excess} ./
 while read a;do grep -w ${a} tmp.ind | wc -l | awk -v pop=${a} -v max=${MAX} '{if($1 > max)print pop" "$1-max}'; done < extract.poplist > remove.counts
 python remove_excess.py > tmp.edit.ind
 for i in "extract.par";do
@@ -83,5 +98,5 @@ for version in 1 2;do
     mkdir pdf${version} ; mv *.pdf pdf${version}
 done
 
-zip -r admixture.zip pdf1 pdf2 *sh *py CV_error.txt result.out poplist.txt
-notify ADMIXTURE
+prefix=$(basename ${work_dir})
+zip -r ${prefix}.zip pdf1 pdf2 *.{py,sh,txt} result.out
